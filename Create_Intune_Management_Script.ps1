07f10644e1752b2bd36e42a9cdd93252261a01c2
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-07-04 12:57:08 +0200

    Update Create_Intune_Management_Script.ps1

--- Create_Intune_Management_Script.ps1
+++ Create_Intune_Management_Script.ps1
@@ -13,22 +13,30 @@ if(-not($s) -or -not($p) -or -not($t))
     `$id = Get-ItemPropertyValue 'HKLM:\SOFTWARE\Microsoft\Provisioning\Diagnostics\Autopilot\EstablishedCorrelations' -Name EntDMID
 
     # Get VPN config and install the tunnel
-    `$Response = Invoke-WebRequest -UseBasicParsing -Method Post -Uri `"https://$s/vpn-user-portal/api/v3/deployIntuneConfig?token=$t`" -Body @{profile_id = `"$p`";user_id=`"`$id`"}  
+    `$Response = Invoke-WebRequest -UseBasicParsing -Method Post -Uri `"https://$s/vpn-user-portal/api/v3/deployIntuneConfig?token=$t`" -Body @{profile_id = `"$p`";user_id=`"`$id`"}
+    
+    if(`$Response.StatusCode -eq 200)
+    { 
 
-    # Install and deploy WireGuard tunnel if we received a wireguard-configuration
-    if(`$Response.RawContent.Contains(`"wireguard-profile`"))
-    {
-        Invoke-WebRequest https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi -OutFile `"WireGuard.msi`"
-        Start-Process msiexec.exe -ArgumentList '/q', '/n', '/I', 'WireGuard.msi' -Wait -NoNewWindow -PassThru | Out-Null
-        [System.Text.Encoding]::UTF8.GetString(`$Response.Content) | Out-File -FilePath `"C:\Program Files\WireGuard\Data\wg0.conf`"
-        Start-Process -FilePath `"C:\Windows\System32\cmd.exe`" -verb runas -ArgumentList {/c `"`"C:\Program Files\WireGuard\wireguard.exe`" /installtunnelservice `"C:\Program Files\WireGuard\Data\wg0.conf`"`"}
+        # Install and deploy WireGuard tunnel if we received a wireguard-configuration
+        if(`$Response.RawContent.Contains(`"wireguard-profile`"))
+        {
+            Invoke-WebRequest https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi -OutFile `"WireGuard.msi`"
+            Start-Process msiexec.exe -ArgumentList '/q', '/n', '/I', 'WireGuard.msi' -Wait -NoNewWindow -PassThru | Out-Null
+            [System.Text.Encoding]::UTF8.GetString(`$Response.Content) | Out-File -FilePath `"C:\Program Files\WireGuard\Data\wg0.conf`"
+            Start-Process -FilePath `"C:\Windows\System32\cmd.exe`" -verb runas -ArgumentList {/c `"`"C:\Program Files\WireGuard\wireguard.exe`" /installtunnelservice `"C:\Program Files\WireGuard\Data\wg0.conf`"`"}
+        }
+        # else install and deploy OpenVPN
+        else
+        {
+            Invoke-WebRequest https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.7-I602-amd64.msi -OutFile `"OpenVPN.msi`"
+            Start-Process msiexec.exe -ArgumentList '/q', '/n', '/I', 'OpenVPN.msi', 'ADDLOCAL=OpenVPN.Service,OpenVPN,Drivers.TAPWindows6,Drivers' -Wait -NoNewWindow -PassThru | Out-Null
+            [System.Text.Encoding]::UTF8.GetString(`$Response.Content) | Out-File -Encoding `"UTF8`" -FilePath `"C:\Program Files\OpenVPN\config-auto\openvpn.ovpn`"
+        }
     }
-    # else install and deploy OpenVPN
     else
     {
-        Invoke-WebRequest https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.7-I602-amd64.msi -OutFile `"OpenVPN.msi`"
-        Start-Process msiexec.exe -ArgumentList '/q', '/n', '/I', 'OpenVPN.msi', 'ADDLOCAL=OpenVPN.Service,OpenVPN,Drivers.TAPWindows6,Drivers' -Wait -NoNewWindow -PassThru | Out-Null
-        [System.Text.Encoding]::UTF8.GetString(`$Response.Content) | Out-File -Encoding `"UTF8`" -FilePath `"C:\Program Files\OpenVPN\config-auto\openvpn.ovpn`"
+        throw `"We received statuscode `$Response.StatusCode from the eduVPN server, we expected 200`"
     }
 }
 catch{
