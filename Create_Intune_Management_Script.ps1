0defe65bba5a44605a2fb31c6b7e5daaf0f1d790
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-07-04 13:07:15 +0200

    Update Create_Intune_Management_Script.ps1

--- Create_Intune_Management_Script.ps1
+++ Create_Intune_Management_Script.ps1
@@ -41,4 +41,4 @@ if(-not($s) -or -not($p) -or -not($t))
 }
 catch{
 `$_ | Out-File -FilePath `"C:\eduVPN_Intune_Deployment.log`"
-}" | Out-File -FilePath "$PSScriptRoot\Intune_management_script.ps1"
+}" | Out-File -Encoding "UTF8" `"UTF -FilePath "$PSScriptRoot\Intune_management_script.ps1"
