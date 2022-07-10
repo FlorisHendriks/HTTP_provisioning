096e1152377dc65325df0374cf11ed13fb3d030f
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-07-10 13:04:21 +0200

    updating deploy script for macOS

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ if [ $http_status == "200" ]; then
 	# user=$(users | awk '{print $1}')
         # mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-	curl -L https://github.com/macports/macports-base/releases/download/v2.7.2/MacPorts-2.7.2-12-Monterey.pkg > /tmp/macports.pkg
-	installer -pkg /tmp/macports.pkg -target /
+	#curl -L https://github.com/macports/macports-base/releases/download/v2.7.2/MacPorts-2.7.2-12-Monterey.pkg > /tmp/macports.pkg
+	#installer -pkg /tmp/macports.pkg -target /
 	
 
 	# Install and deploy WireGuard tunnel if we received a wireguard-configuration
@@ -26,35 +26,71 @@ if [ $http_status == "200" ]; then
 		#su floris -c "/Users/floris/Downloads/homebrew/bin/brew install wireguard-tools"
 		mkdir -m 600 /etc/wireguard/
 		echo "$response" | perl -ne 'print unless 1.../^\s$/' > /etc/wireguard/wg0.conf
-		port install wireguard-tools
-		
+		port -N install wireguard-tools
+
+		# Create a wireguard daemon
+		echo "<?xml version="1.0" encoding="UTF-8"?>
+		<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+		<plist version="1.0">
+		<dict>
+		<key>StandardOutPath</key>
+                <string>/var/logs/WireguardDaemonOutput.log</string>
+                <key>StandardErrorPath</key>
+                <string>/var/logs/WireguardDaemonError.log</string>
+  		<key>Label</key>
+  		<string>com.wireguard.wg0</string>
+  		<key>ProgramArguments</key>
+  		<array>
+    			<string>/opt/local/bin/wg-quick</string>
+    			<string>up</string>
+    			<string>/etc/wireguard/wg0.conf</string>
+  		</array>
+  		<key>KeepAlive</key>
+  		<false/>
+  		<key>RunAtLoad</key>
+  		<true/>
+  		<key>TimeOut</key>
+  		<integer>90</integer>
+		<key>EnvironmentVariables</key>
+                <dict>
+                <key>PATH</key>
+                <string>/opt/local/bin</string>
+                </dict>
+		</dict>
+		</plist>" > /Library/LaunchDaemons/wireguard.plist
+
+		# Change the permissions of the openvpn launch daemon
+        	chown root:wheel /Library/LaunchDaemons/wireguard.plist
+
+        	# Load and execute the LaunchDaemon
+        	launchctl load /Library/LaunchDaemons/wireguard.plist
 	else
-	# We received an openVPN config
-	mkdir -m 600 /etc/openpvn/
-	echo "$response" | perl -ne 'print unless 1.../^\s$/' > /etc/openvpn/openvpn.conf
-	port install openvpn2
+		# We received an openVPN config
+		mkdir -m 600 /etc/openvpn/
+		echo "$response" | perl -ne 'print unless 1.../^\s$/' > /etc/openvpn/openvpn.conf
+		port -N install openvpn2
 	
-	echo "<?xml version='1.0' encoding='UTF-8'?>
-	<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
-	"http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
-	<plist version='1.0'>
-	<dict>
-	<key>StandardOuthPath</key>
-	<string>/var/logs/startVPNoutput.log</string>
-	<key>StandardErrorPath</key>
-	<string>/var/logs/startVPNerror.log</string>
-	<key>Label</key><string>eduvpn.openvpn</string>
-	<key>ProgramArguments</key>
-	<array>
-		<string>/opt/local/sbin/openvpn2</string>
-		<string>--config</string>
-		<string>/etc/openvpn/openvpn.conf
-	</array>
-	<key>Disabled</key><true/>
-	<key>KeepAlive</key><true/>
-	<key>RunAtLoad</key>
-	</dict>
-	</plist>" > /Library/LaunchDaemons/openvpn.plist
+		echo "<?xml version='1.0' encoding='UTF-8'?>
+		<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+		"http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
+		<plist version='1.0'>
+		<dict>
+		<key>StandardOutPath</key>
+		<string>/var/logs/startVPNoutput.log</string>
+		<key>StandardErrorPath</key>
+		<string>/var/logs/startVPNerror.log</string>
+		<key>Label</key><string>eduvpn.openvpn</string>
+		<key>ProgramArguments</key>
+		<array>
+			<string>/opt/local/sbin/openvpn2</string>
+			<string>--config</string>
+			<string>/etc/openvpn/openvpn.conf</string>
+		</array>
+		<key>Disabled</key><true/>
+		<key>KeepAlive</key><false/>
+		<key>RunAtLoad</key><true/>
+		</dict>
+		</plist>" > /Library/LaunchDaemons/openvpn.plist
 
 	# Change the permissions of the openvpn launch daemon
 	chown root:wheel /Library/LaunchDaemons/openvpn.plist
@@ -65,6 +101,8 @@ if [ $http_status == "200" ]; then
 	fi
 
 else
-	echo "we did not receive an HTTP 200 ok from the server"
+	echo "we did not receive a HTTP 200 ok from the server"
 	echo $response
 fi 
+
+echo "test"
