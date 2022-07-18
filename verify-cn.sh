c7a4c3605c66d5004eb8e30977de01db6d766de4
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-07-18 11:06:29 +0200

    verify-cn changes

--- verify-cn.sh
+++ verify-cn.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-depth = "$0"
-certificate = "$1"
+depth="$1"
+commonName="$2"
 
 if [ "$depth" -eq 0 ]; then
         # Parse out the common name substring in the X509 subject string.
-        clientCN="$(openssl x509 -noout -subject -in "$1" -nameopt multiline | sed -n 's/ *commonName *= //p')"
+        clientCN="$(echo "$commonName" |  sed 's/^.*CN=//')"
 
         # Get the Intune token so that we are allowed to do api calls
-        postParams='client_id=client_id&scope=https://graph.microsoft.com/.default&client_secret=client_secret_placeholder&grant_type=client_credentials'
+        postParams='client_id=client_id&scope=https://graph.microsoft.com/.default&client_secret=client_scret_placeholder&grant_type=client_credentials'
         tokenResponse="$(curl -X POST 'https://login.microsoftonline.com/a9d8059a-38d4-4690-bdd4-9f7d0662d8d0/oauth2/v2.0/token' -X POST -d "$postParams")"
         token="$(echo "$tokenResponse" | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')"
 
         # Receive the managed device ids of the Intune tenant
         url='https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?$select=id'
         response="$(curl -X GET "$url" -H "Authorization: Bearer $token" -H 'Content-Type: application/json' -H 'ConsistencyLevel: eventual')"
-        managedIds="$(echo "$response" | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$')"
-
+        managedIds="$(echo "$response" | grep -o '"id":"[^"]*' | grep -o '[^"]*$')"
 
         # Accept the connection if the X509 common name
         # string matches the managed device Id of Intune.
