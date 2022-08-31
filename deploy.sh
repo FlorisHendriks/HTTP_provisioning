52df56a8ca4787e49d2f989b2f08006512150046
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-08-31 13:47:23 +0200

    Add support for Intune certificates

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 # Catch errors and log it to /Library/Logs/Microsoft/eduVpnDeployment.log
 set -e
 trap 'catch $? $LINENO' EXIT
@@ -10,22 +9,21 @@ catch() {
 }
 # Get the managed device id
 id=$(security find-certificate -a | awk -F= '/issu/ && /MICROSOFT INTUNE MDM DEVICE CA/ { getline; print $2 }')
+id=$(echo $id | tr -d '"')
+echo "$id"
 
 # Retrieve config from eduVPN
-response=$(curl -o - -i -s -X POST "https://vpn.strategyit.nl/vpn-user-portal/api/v3/deployIntuneConfig?token=256bit_token_placeholder" -d "profile_id=default&user_id=$id")
+response=$( CURL_SSL_BACKEND=secure-transport curl -s -i --cert "$id" -d "profile_id=routeAlles&user_id=$id" "https://test5.strategyit.nl/")
 
 http_status=$(echo "$response" | awk 'NR==1 {print $2}')
 
-if [ $http_status == "200" ]; then
+if [ "$http_status" == "200" ]; then
 	# Install the latest Macports version, which is a package manager for macOS
-	
+
 	version=$( curl -fs --url 'https://raw.githubusercontent.com/macports/macports-base/master/config/RELEASE_URL' )
 	version=${version##*/v}
-
     	curl -L -O --url "https://github.com/macports/macports-base/releases/download/v${version}/MacPorts-${version}.tar.gz"
-
 	tar -zxf   MacPorts-${version}.tar.gz 2>/dev/null
-
 	cd MacPorts-${version}
 	CC=/usr/bin/cc ./configure \
      	--prefix=/opt/local \
@@ -35,27 +33,22 @@ if [ $http_status == "200" ]; then
      	--enable-readline \
 	&& make SELFUPDATING=1 \
 	&& make install SELFUPDATING=1
-
+	
 	# update MacPorts itself
 	/opt/local/bin/port -dN selfupdate
 	
 	# cleanup
 	cd ..
 	rm  -rf  ./MacPorts-${version}	
-
+	
 	# Install and deploy WireGuard tunnel if we received a wireguard-configuration
-	vpnProtocol=$(echo "$response" | awk -F':' '/Content-Type/ {print $2}')
-	vpnProtocol="$(echo "$vpnProtocol" | tr -d '[:space:]')"
-
-	if [ "$vpnProtocol" == "application/x-wireguard-profile" ]; then
+	if [[ "$response" == *"Interface"* ]]; then
 		echo "wireguard"
-		#su floris -c "/Users/floris/Downloads/homebrew/bin/brew install wireguard-tools"
 		if [ ! -e /etc/wireguard ]; then
 			mkdir -m 600 /etc/wireguard/
 		fi
 		echo "$response" | perl -ne 'print unless 1.../^\s$/' > /etc/wireguard/wg0.conf
 		port -N install wireguard-tools
-
 		# Create a wireguard daemon
 		echo "<?xml version="1.0" encoding="UTF-8"?>
 		<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -86,10 +79,9 @@ if [ $http_status == "200" ]; then
                 </dict>
 		</dict>
 		</plist>" > /Library/LaunchDaemons/wireguard.plist
-
+		
 		# Change the permissions of the openvpn launch daemon
         	chown root:wheel /Library/LaunchDaemons/wireguard.plist
-
         	# Load and execute the LaunchDaemon
         	launchctl load /Library/LaunchDaemons/wireguard.plist
 	else
@@ -122,15 +114,13 @@ if [ $http_status == "200" ]; then
                 <integer>90</integer>
 		</dict>
 		</plist>" > /Library/LaunchDaemons/openvpn.plist
-
-	# Change the permissions of the openvpn launch daemon
-	chown root:wheel /Library/LaunchDaemons/openvpn.plist
+		
+		# Change the permissions of the openvpn launch daemon
+		chown root:wheel /Library/LaunchDaemons/openvpn.plist
 	
-	# Load and execute the LaunchDaemon
-	# launchctl load /Library/LaunchDaemons/openvpn.plist
-
+		# Load and execute the LaunchDaemon
+		launchctl load /Library/LaunchDaemons/openvpn.plist
 	fi
-
 else
 	echo "we did not receive a HTTP 200 ok from the server" > /Library/Logs/Microsoft/eduVpnDeployment.log
 	echo $response > /Library/Logs/Microsoft/eduVpnDeployment.log
