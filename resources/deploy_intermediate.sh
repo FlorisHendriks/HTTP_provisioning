95398f613b1b0cc665787765bc81080b0043d739
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-09-05 12:56:21 +0200

    Update deploy_intermediate.sh

--- resources/deploy_intermediate.sh
+++ resources/deploy_intermediate.sh
@@ -79,5 +79,6 @@ cp ./revokeVpnConfigs "/etc/eduVpnProvisioning/revokeVpnConfigs"
 sed -i "s/{applicationId}/${APPLICATION_ID}/" "/etc/eduVpnProvisioning/revokeVpnConfigs"
 sed -i "s/{secretToken}/${SECRET_TOKEN}/" "/etc/eduVpnProvisioning/revokeVpnConfigs"
 sed -i "s/{adminApiToken}/${ADMIN_API_TOKEN}/" "/etc/eduVpnProvisioning/revokeVpnConfigs"
+sed -i "s/vpn.example/${VPN_FQDN}/" "/var/www/${VPN_FQDN}/index.php"
 
 cp ./eduVpnProvisioning /etc/cron.d/eduVpnProvisioning
