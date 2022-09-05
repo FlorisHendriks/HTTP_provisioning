443d4125d699e8494c90dfc57c861dfa2cae38da
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-09-05 11:23:40 +0200

    Update Create_Intune_Management_Script.ps1

--- Create_Intune_Management_Script.ps1
+++ Create_Intune_Management_Script.ps1
@@ -51,9 +51,8 @@ if(-not($s) -or -not($p))
         else
         {
             .\winget.exe install OpenVPNTechnologies.OpenVPN --silent --accept-package-agreements --accept-source-agreements --override `"ADDLOCAL=OpenVPN.Service,OpenVPN,Drivers.TAPWindows6,Drivers`"
-            Start-Process msiexec.exe -ArgumentList '/q', '/n', '/I', 'OpenVPN.msi', 'ADDLOCAL=OpenVPN.Service,OpenVPN,Drivers.TAPWindows6,Drivers' -Wait -NoNewWindow -PassThru | Out-Null
             `$Response.Content | Out-File -Encoding `"UTF8`" -FilePath `"C:\Program Files\OpenVPN\config-auto\openvpn.ovpn`"
-	    Restart-Service -Name OpenVPNService
+            Restart-Service -Name OpenVPNService
         }
     }
     else
