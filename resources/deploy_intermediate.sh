0a09d29973d488fc5a5b046fd023585de67b4080
FlorisHendriks98 <47246332+FlorisHendriks98@users.noreply.github.com>
2022-09-06 07:45:27 +0200

    Update deploy_intermediate.sh

--- resources/deploy_intermediate.sh
+++ resources/deploy_intermediate.sh
@@ -76,6 +76,7 @@ systemctl restart apache2
 mkdir -m 600 /etc/eduVpnProvisioning
 cp ./revokeVpnConfigs "/etc/eduVpnProvisioning/revokeVpnConfigs"
 touch /etc/eduVpnProvisioning/localDeviceIds.txt
+chmod 666 /etc/eduVpnProvisioning/localDeviceIds.txt
 
 sed -i "s/{applicationId}/${APPLICATION_ID}/" "/etc/eduVpnProvisioning/revokeVpnConfigs"
 sed -i "s/{secretToken}/${SECRET_TOKEN}/" "/etc/eduVpnProvisioning/revokeVpnConfigs"
